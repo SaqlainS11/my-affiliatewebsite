28592f078f4f71460ed6392a349ba533588da1b7
SaqlainS11 <161482427+SaqlainS11@users.noreply.github.com>
2024-02-28 17:05:38 +0500

    Add files via upload

---
+++ send-email.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "your@email.com";
+    $subject = "New Inquiry from Fashion & Beauty Affiliate Marketing Website";
+    $body = "Name: $name\nEmail: $email\n\n$message";
+
+    if (mail($to, $subject, $body)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error sending message!";
+    }
+}
+?>
